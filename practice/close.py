0bf736aab6052cab3bf608a508fb184d865d336b
xiangxu <xiangxu@datagrand.com>
2022-08-15 13:55:24 +0800

    feature helloworld done

--- practice/close.py
+++ practice/close.py
@@ -1,5 +1,8 @@
 import sys
 import time
 
-time_now = time.strftime(%Y-%m-%d, time.localtime(time.time()))
+time_now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+print(time_now)
 sys.exit()
+
+print(time.strptime(time_now, '%Y/%m/%d'))
